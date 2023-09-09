2bc686d5f177b38e19e4949a31566bf4019ea9b1
Katia <kushnir.ekaterina@gmail.com>
2023-09-09 21:29:52 +0200

    Add integration test

--- src/App.test.js
+++ src/App.test.js
@@ -1,5 +1,6 @@
 import { render } from "@testing-library/react"
 import ListNotes from "./components/ListNotes"
+import App from "./App";
 
 describe('Testing components', () => {
   test('if the list renders correctly', () => {
@@ -11,5 +12,18 @@ describe('Testing components', () => {
     const r = render(<ListNotes notes={notes} />);
     const div = r.getByLabelText('list-notes');
     expect(div.childElementCount).toBe(2);
+  })
+})
+
+describe('Integration test', () => {
+  test('if renders the app', () => {
+    const r = render(<App />);
+    expect(r).toBeDefined();
+  })
+  test('if renders the input', () => {
+    const placeholdertext = "Please input a note";
+    const r = render(<App />);
+    const input = r.getByPlaceholderText(placeholdertext);
+    expect(input).toBeDefined();
   })
 })
